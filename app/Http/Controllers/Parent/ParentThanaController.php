05b19268bed43eb6702e4ecf13182f510f0abf7e
tasnim-jawad <tasnimulh453@gmail.com>
2025-05-07 17:56:03 +0600

    parent thana migration

---
+++ app/Http/Controllers/Parent/ParentThanaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Parent;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ParentThanaController extends Controller
+{
+    //
+}
---
+++ app/Models/Parent/ParentThana.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Parent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ParentThana extends Model
+{
+    //
+}
